65f7840bb45dd0c2d8d5bd15091ccf735aa6750d
chengwei0815 <chengwei0815@gmail.com>
2023-06-15 19:44:46 -0400

    edit alert

--- public/script/script.js
+++ public/script/script.js
@@ -7,3 +7,17 @@ function collapse_menu() {
 function uncollapse_menu() {
     menu_link.style.right = "-200px";
 }
+
+function confirmDelete() {
+  // alert('Click OK if you want to delete the course')
+  const formElement = document.getElementById("deleteCoursesForm");
+  const formElementServices = document.getElementById("deleteServicesForm");
+  // console.log(formElement);
+  const userResponse = confirm("Are you sure?");
+  if (userResponse === true) {
+    formElement.submit();
+    formElementServices.submit();
+  } else {
+    return false;
+  }
+}
